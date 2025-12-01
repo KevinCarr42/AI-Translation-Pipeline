47a5c4745dbe7a4f39879b44b2b26481075ffb99
Kevin <kevincarr@gmail.com>
2025-12-01 13:15:59 -0400

    redo main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import config
+from data_cleaning import data_cleaning_pipeline
+from model_finetuning import finetuning_pipeline
+from translate import translation_pipeline
+
+
+if __name__ == "__main__":
+    clean_data = True
+    finetune_models = False
+    translate_data = False
+    with_preferential_translation = True
+    
+    if clean_data:
+        data_cleaning_pipeline()
+    
+    if finetune_models:
+        finetuning_pipeline()
+    
+    if translate_data:
+        translation_pipeline(with_preferential_translation=with_preferential_translation)
