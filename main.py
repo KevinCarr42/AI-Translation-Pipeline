81cea2111f28ee9dd3f898b18e0944e5af41ba01
Kevin <kevincarr@gmail.com>
2025-12-02 10:11:10 -0400

    partial cleanup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import config
+
 from data_cleaning import data_cleaning_pipeline
 from model_finetuning import finetuning_pipeline
 from translate import translation_pipeline
@@ -7,12 +10,20 @@ if __name__ == "__main__":
     clean_data = True
     finetune_models = False
     translate_data = False
-    with_preferential_translation = True
     
     if clean_data:
-        data_cleaning_pipeline()
+        correlation_csv_path = os.path.join(config.DATA_DIR, "fr_eng_correlation_data.csv")
+        parsed_docs_folder = os.path.join(config.DATA_DIR, "ParsedPublications")
+        
+        data_cleaning_pipeline(
+            correlation_csv_path=correlation_csv_path,
+            parsed_docs_folder=parsed_docs_folder,
+            linebreaks=True,
+            add_features=True
+        )
     
     if finetune_models:
+        with_preferential_translation = True
         finetuning_pipeline()
     
     if translate_data:
