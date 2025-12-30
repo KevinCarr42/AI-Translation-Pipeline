c56e94ccf154b3a0fb894fb3cf8a5d6be366cf32
Kevin <kevincarr@gmail.com>
2025-12-30 10:20:56 -0400

    change setup

--- evaluate_quality.py
+++ evaluate_quality.py
@@ -3,7 +3,8 @@ from quality_evaluation.evaluate import run_quality_evaluation
 if __name__ == '__main__':
     jsonl_path = "../Data/pipeline_testing_data.jsonl"
     results_output_file = "quality_evaluation/tests/quality_test_results.pickle"
-    n_samples = 10
+    seed = 42
+    n_samples = 100
     models_to_use = [
         'opus_mt_base', 'opus_mt_finetuned',
         'm2m100_418m_base', 'm2m100_418m_finetuned',
@@ -14,6 +15,7 @@ if __name__ == '__main__':
         models_to_use=models_to_use,
         jsonl_path=jsonl_path,
         n_samples=n_samples,
+        seed=seed,
         use_find_replace=True,
         compare_find_replace=True,
         output_pickle=results_output_file
